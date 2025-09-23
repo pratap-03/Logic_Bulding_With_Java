4c909d3203e2eeb90e1103daf3315c1049aae221
pratap-03 <gaikwadpratap39@gmail.com>
2025-09-23 09:42:05 +0530

    Day1_Pattern

---
+++ Day1_Pattern.java
@@ -0,0 +1,50 @@
+public class Day1_Pattern {
+    public static void main(String[] args) {
+        for(int z=5;z>=0;z--) {
+            for (int h = 0; h <= z; h++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+         for(int i=0;i<=5;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+ }
+//        for(int z=5;z>=0;z--) {
+//            for (int h = 0; h <= z; h++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//         for(int i=0;i<=5;i++){
+//            for (int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }for(int z=5;z>=0;z--) {
+//            for (int h = 0; h <= z; h++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//         for(int i=0;i<=5;i++){
+//            for (int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }for(int z=5;z>=0;z--) {
+//            for (int h = 0; h <= z; h++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//         for(int i=0;i<=5;i++){
+//            for (int j=0;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+    }
+}
